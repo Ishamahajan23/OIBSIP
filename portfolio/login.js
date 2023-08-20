2de4efd3122ae3a54e1ecc24522cf6221ba7006b
Ishamahajan23 <108974410+Ishamahajan23@users.noreply.github.com>
2023-08-20 13:34:36 +0530

    Add files via upload

---
+++ portfolio/login.js
@@ -0,0 +1,15 @@
+var a=document.querySelectorAll("span");
+var btn=document.querySelector(".button");
+
+let arr=["Isha", "Student","Engineer","Fresher"];
+
+
+span.addEventListener("auto",function(){
+    for(let i=0;i<arr.length;i++){
+        span.innerHtml=arr[i];
+
+
+    }
+
+    
+})
